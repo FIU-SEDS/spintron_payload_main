f2e2f205db0e4648d16adf19505607b6d272ed9a
Luis Canada <canadaluis5@gmail.com>
2025-06-01 00:45:48 -0400

    Change to using L298N and update wiring diagram

--- motor.ino
+++ motor.ino
@@ -1,17 +1,28 @@
-const int MOTOR_PIN = 3;
+const int MOTOR_PIN_1 = 3;
+const int MOTOR_PIN_2 = 46;
+const int MOTOR_PIN_PWM = 9;
 
 const int MOTOR_SPEED = 300; // Maximum speed (4095)
 
 void setupMotor() {
-  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(MOTOR_PIN_1, OUTPUT);
+  pinMode(MOTOR_PIN_2, OUTPUT);
+  pinMode(MOTOR_PIN_PWM, OUTPUT);
+
   turnOffMotor();
 }
 
 void turnOnMotor() {
-  analogWrite(MOTOR_PIN, MOTOR_SPEED);
+  digitalWrite(MOTOR_PIN_1, HIGH);
+  digitalWrite(MOTOR_PIN_2, LOW);
+
+  digitalWrite(MOTOR_PIN_PWM, MOTOR_SPEED);
 }
 
 
 void turnOffMotor() {
-  analogWrite(MOTOR_PIN, 0);
+  digitalWrite(MOTOR_PIN_1, LOW);
+  digitalWrite(MOTOR_PIN_2, LOW);
+
+  digitalWrite(MOTOR_PIN_PWM, 0);
 }
